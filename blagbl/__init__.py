b729849e0596c38d66942d3f92600a3d380e146e
Wes Hardaker <opensource@hardakers.net>
2025-03-28 10:40:01 -0700

    version bump

--- blagbl/__init__.py
+++ blagbl/__init__.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 from logging import info, error, warning
 from pathlib import Path
 
-__VERSION__ = "0.1"
+__VERSION__ = "0.1.1"
 
 CACHE_VERSION: int = 1
 DEFAULT_STORE: Path = Path(os.environ["HOME"]).joinpath(".local/share/blag")
